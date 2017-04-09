18a124e8521021d8fbeec7e04ca0a6c459247ab3
inaz2 <inaz2@users.noreply.github.com>
2017-04-09 14:03:01 +0900

    rsautil.rb: implement generate command

--- rsautil.rb
+++ rsautil.rb
@@ -1,10 +1,24 @@
 require 'openssl'
 
-if ARGV[0] == 'dump' then
+if ARGV[0] == 'parse' then
   rsa = OpenSSL::PKey::RSA.new(File.read(ARGV[1]))
   puts "n = #{rsa.n}"
   puts "e = #{rsa.e}"
-elsif ARGV[0] == 'dec' then
+elsif ARGV[0] == 'generate' then
+  rsa = OpenSSL::PKey::RSA.new()
+  print 'p? '
+  rsa.p = STDIN.gets.to_i
+  print 'q? '
+  rsa.q = STDIN.gets.to_i
+  print 'e? '
+  rsa.e = STDIN.gets.to_i
+  rsa.n = rsa.p * rsa.q
+  rsa.d = rsa.e.mod_inverse((rsa.p-1) * (rsa.q-1))
+  rsa.dmp1 = rsa.d % (rsa.p-1)
+  rsa.dmq1 = rsa.d % (rsa.q-1)
+  rsa.iqmp = rsa.q.mod_inverse(rsa.p)
+  puts rsa.to_pem
+elsif ARGV[0] == 'decrypt' then
   rsa = OpenSSL::PKey::RSA.new()
   print 'p? '
   rsa.p = STDIN.gets.to_i
@@ -19,6 +33,6 @@ elsif ARGV[0] == 'dec' then
   rsa.iqmp = rsa.q.mod_inverse(rsa.p)
   puts rsa.private_decrypt(File.read(ARGV[1]))
 else
-  puts "Usage: ruby #{$0} (dump PEM_FILE | dec ENCRYPTED_FILE)"
+  puts "Usage: ruby #{$0} (parse PEM_FILE | generate | decrypt ENCRYPTED_FILE)"
   abort
 end
